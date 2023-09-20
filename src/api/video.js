2261f863b8f73025e246e4c1962497d56306ae34
cookiekingjlim <133620407+cookiekingjlim@users.noreply.github.com>
2023-09-20 15:58:51 +0900

    feat: 부트스트랩을 이용한 파일 업로드 구현

---
+++ src/api/video.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+// http://localhost:8080/api/
+const instance = axios.create({
+    baseURL: "http://localhost:8000/api",
+});
+
+export const getCategories = async () => {
+    return await instance.get("category");
+
+};
+
--- src/router.js
+++ src/router.js
@@ -2,11 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Watch from "./pages/Watch";
 import Layout from "./components/Layout";
+import NotFound from "./pages/NotFound";
+import Create from "./pages/Create";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
+        errorElement: <NotFound />,
         children: [
             {
                 index: true,
@@ -16,7 +19,12 @@ const router = createBrowserRouter([
                 path: "/watch",
                 element: <Watch/>
             },
+        
         ]
+    },
+    {
+        path: "/create",
+        element: <Create />,
     }
     
 ]);
